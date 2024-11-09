cee8c9ec564e1c1da682e47c95ae9bf24b4ae72b
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-11-09 19:39:23 +0100

    Chapter 3 - Adding and optimizing fonts and images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Raleway } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["900"],
+  style: ["normal"],
+  display: "swap",
+});
