6798704d8c49935d070ffcb4d44559ad630914c9
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2024-11-10 10:39:36 +0100

    font error fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,7 @@
-import { Inter, Raleway } from "next/font/google";
+import { Inter, Raleway, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400"] });
 export const raleway = Raleway({
   subsets: ["latin"],
   weight: ["900"],
